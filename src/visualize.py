d1f890a8bd87d0bec6947e0bee27eaba6233cb7a
heehyunwon <2517024@ewha.ac.kr>
2026-08-09 11:15:10 +0900

    Add single-channel EEG visualization

---
+++ src/visualize.py
@@ -0,0 +1,33 @@
+import mne
+import matplotlib.pyplot as plt
+
+# EEG 데이터 불러오기
+raw = mne.io.read_raw_edf(
+    "data/S001R01.edf",
+    preload=True
+)
+
+data = raw.get_data()
+first_channel = data[0]
+
+sampling_rate = raw.info["sfreq"] # Hz
+time = range(len(first_channel))
+time = [t / sampling_rate for t in time]
+
+plt.figure(figsize=(12, 4))
+plt.plot(time, first_channel)
+
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude (V)")
+channel_name = raw.ch_names[0]
+plt.title(f"EEG Signal - {channel_name}")
+
+plt.tight_layout()
+
+plt.savefig("results/single_channel.png", dpi=300)
+
+plt.show()
+
+print("첫 번째 채널: ", raw.ch_names[0])
+
+
